8b76c776f7cc9ded6325d2e4e0f17897b4f9afd9
oacikel <41515954+oacikel@users.noreply.github.com>
2020-12-12 14:45:36 +0300

    Tab Bar Added
    A tab bar added to iterrate through camera types for each rover.
    
    No functionality has been added to the tabs yet.

--- lib/model/enum.dart
+++ lib/model/enum.dart
@@ -14,4 +14,40 @@ enum eCamera {
   NAVCAM,
   PANCAM,
   MINITES
+
 }
+
+String getCameraName(eCamera camera){
+  switch (camera){
+
+    case eCamera.FHAZ:
+      return "Front Hazard Avoidance Camera";
+      break;
+    case eCamera.RHAZ:
+      return "Rear Hazard Avoidance Camera";
+      break;
+    case eCamera.MAST:
+      return "Mast Camera";
+      break;
+    case eCamera.CHEMCAM:
+      return "Chemistry and Camera Complex";
+      break;
+    case eCamera.MAHLI:
+      return "Mars Hand Lens Imager";
+      break;
+    case eCamera.MARDI:
+      return "Mars Descent Imager";
+      break;
+    case eCamera.NAVCAM:
+      return "Navigation Camera";
+      break;
+    case eCamera.PANCAM:
+      return "Panoramic Camera";
+      break;
+    case eCamera.MINITES:
+      return "Miniature Thermal Emission";
+      break;
+  }
+}
+
+
--- lib/views/home_widget.dart
+++ lib/views/home_widget.dart
@@ -1,3 +1,4 @@
+import 'package:eyes_of_rovers/model/enum.dart';
 import 'package:eyes_of_rovers/model/mockData.dart';
 import 'package:flutter/material.dart';
 import 'package:eyes_of_rovers/constants/Constants.dart' as CONSANTS;
@@ -12,43 +13,83 @@ class _HomeState extends State<Home> {
 
   int _currentIndex = 0;
 
-   final List<Widget> _children = [
-    NasaImageWidget(Key(CONSANTS.bottom_nav_bar_item_curiosity),MockData.getMockCuriosityImages()),
-    NasaImageWidget(Key(CONSANTS.bottom_nav_bar_item_opportunity),MockData.getMockOpportunityImages()),
-    NasaImageWidget(Key(CONSANTS.bottom_nav_bar_item_spirit),MockData.getMockSpritImages())
+  final List<Widget> _children = [
+    NasaImageWidget(Key(CONSANTS.bottom_nav_bar_item_curiosity),
+        MockData.getMockCuriosityImages()),
+    NasaImageWidget(Key(CONSANTS.bottom_nav_bar_item_opportunity),
+        MockData.getMockOpportunityImages()),
+    NasaImageWidget(
+        Key(CONSANTS.bottom_nav_bar_item_spirit), MockData.getMockSpritImages())
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(CONSANTS.app_name),
-      ),
-      body: _children[_currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        onTap: onTabTapped,
-        currentIndex: _currentIndex,
-        // this will be set when a new tab is tapped
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.terrain),
-            title: new Text(CONSANTS.bottom_nav_bar_item_curiosity),
+    return DefaultTabController(
+      length: _generateTabs().length,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text(CONSANTS.app_name),
+          bottom: TabBar(
+            isScrollable: true,
+            tabs:_generateTabs(),
           ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.android),
-            title: new Text(CONSANTS.bottom_nav_bar_item_opportunity),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.airplanemode_active),
-              title: Text(CONSANTS.bottom_nav_bar_item_spirit))
-        ],
+        ),
+        body: _children[_currentIndex],
+        bottomNavigationBar: BottomNavigationBar(
+          onTap: _onTabTapped,
+          currentIndex: _currentIndex,
+          // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.terrain),
+              title: new Text(CONSANTS.bottom_nav_bar_item_curiosity),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.android),
+              title: new Text(CONSANTS.bottom_nav_bar_item_opportunity),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.airplanemode_active),
+                title: Text(CONSANTS.bottom_nav_bar_item_spirit))
+          ],
+        ),
       ),
     );
   }
 
-  void onTabTapped(int index) {
+  void _onTabTapped(int index) {
     setState(() {
       _currentIndex = index;
     });
+  }
+
+  List<Tab> _generateTabs() {
+    if (_currentIndex == 0) {
+      return [
+        Tab(text: getCameraName(eCamera.FHAZ)),
+        Tab(text: getCameraName(eCamera.RHAZ)),
+        Tab(text: getCameraName(eCamera.MAST)),
+        Tab(text: getCameraName(eCamera.CHEMCAM)),
+        Tab(text: getCameraName(eCamera.MAHLI)),
+        Tab(text: getCameraName(eCamera.MARDI)),
+        Tab(text: getCameraName(eCamera.NAVCAM)),
+      ];
+    } else if (_currentIndex == 1 ) {
+      return [
+        Tab(text: getCameraName(eCamera.FHAZ)),
+        Tab(text: getCameraName(eCamera.RHAZ)),
+        Tab(text: getCameraName(eCamera.NAVCAM)),
+        Tab(text: getCameraName(eCamera.PANCAM)),
+        Tab(text: getCameraName(eCamera.MINITES)),
+      ];
+    } else if (_currentIndex == 2) {
+      return [
+        Tab(text: getCameraName(eCamera.FHAZ)),
+        Tab(text: getCameraName(eCamera.RHAZ)),
+        Tab(text: getCameraName(eCamera.NAVCAM)),
+        Tab(text: getCameraName(eCamera.PANCAM)),
+        Tab(text: getCameraName(eCamera.MINITES)),
+      ];
+    }
   }
 }
